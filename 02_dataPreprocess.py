c95598fb621581243c26f130573ae808b9f7eda3
Murali Tummalapalli <tummalapallimurali@gmail.com>
2025-01-24 07:20:55 -0500

    new changes

--- 02_dataPreprocess.py
+++ 02_dataPreprocess.py
@@ -33,7 +33,7 @@ class preprocess():
         # log start time 
         
     def data_preprocess(self):
-        logging.info('Data Preprocessing Started')
+        logging.info('**Data Preprocessing Initated**')
         self.start_time = pd.Timestamp.now()
         for file in os.listdir(self.dir):
             with open(os.path.join(self.dir, file), 'r') as f:
@@ -42,13 +42,13 @@ class preprocess():
         logging.info(f'Total number of records read: {self.data.shape[0]}')
         logging.info(f'Number of records with NULL values: {self.data.isnull().sum()}')
         self.data = self.data.dropna()
-        logging.info('Data Preprocessing Completed')
         self.end_time = pd.Timestamp.now()
         logging.info(f'Total runtime of the data cleaning phase in milliseconds: {self.end_time - self.start_time}')
+        logging.info('**Data Preprocessing Completed**')
         return self.data
     
     def key_metrics(self):
-        logging.info('Key Metrics Calculation Started')
+        logging.info('**Key Metrics Calculation Started**')
         # logging.info(f'AVG flight_duration_secs for the Top 25 destination cities by the total number of passengers arriving: {self.data.groupby("destination_city")["flight_duration_secs"].mean().nlargest(25)}')
         # logging.info(f'P95 flight_duration_secs for the Top 25 destination cities by the total number of passengers arriving: {self.data.groupby("destination_city")["flight_duration_secs"].apply(lambda x: np.percentile(x, 95)).nlargest(25)}')
         self.data['flight_duration_secs'] = pd.to_numeric(self.data['flight_duration_secs'], errors='coerce')
@@ -60,10 +60,10 @@ class preprocess():
         top_25_destinations = self.data.groupby("destination_city")["flight_duration_secs"].mean().nlargest(25)
 
         logging.info(f'AVG flight_duration_secs for the Top 25 destination cities by the total number of passengers arriving: {top_25_destinations}')
-        logging.info('Key Metrics Calculation Completed')
+        logging.info('**Key Metrics Calculation Completed**')
 
     def passenger_balance_analysis(self):
-        logging.info('Passenger Balance Analysis Started')
+        logging.info('**Passenger Balance Analysis Initiated**')
         passenger_balance = pd.DataFrame(columns=['city', 'passengers'], dtype='int')
         passenger_balance['city'] = self.data['destination_city']
         passenger_balance['passengers'] = self.data['#_of_passengers_on_board']
@@ -71,13 +71,24 @@ class preprocess():
         passenger_balance['passengers'] = passenger_balance['passengers'] - self.data.groupby('origin_city')['#_of_passengers_on_board'].sum()
         logging.info(f'City with the maximum remaining passengers: {passenger_balance["passengers"].idxmax()}')
         logging.info(f'City with the minimum remaining passengers: {passenger_balance["passengers"].idxmin()}')
-        logging.info('Passenger Balance Analysis Completed')
+        logging.info('**Passenger Balance Analysis Completed**')
         return passenger_balance
     
+    # collect all the loggings and update readme file
+    def report(self):
+        with open("README.md", "a") as f:
+            f.write(f'\n\n{pd.Timestamp.now()}:\n')
+            with open('preprocess.log', 'r') as log:
+                f.write(log.read())
+        logging.info('Report Updated')
+
     def run(self):
         self.data_preprocess()
         self.key_metrics()
         self.passenger_balance_analysis()
+        self.report()
+
+    
 
 if __name__=='__main__':
     logging.basicConfig(filename='preprocess.log', level=logging.INFO)
